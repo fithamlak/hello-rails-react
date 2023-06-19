516a1c3d3f696e2211caeeaae47fe05cf0a14b4a
fithamlak <justiceofgod01@gmail.com>
2023-06-20 00:25:42 +0300

    Create an API endpoint that selects a random greeting from your table

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get '/random_greeting', to: 'greetings#random_greeting'
+  root 'static_pages#home'
+
 end
